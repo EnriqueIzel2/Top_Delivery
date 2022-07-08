0d4f3f71ba66cabf2f6a1ae2bb97f1dfa1eb4794
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-07 20:50:03 -0400

    feat: create method to get IDs and listener to navigate to Register

--- app/src/main/java/com/enriqueizel/topdelivery/Form_login.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Form_login.java
@@ -2,9 +2,14 @@ package com.enriqueizel.topdelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Form_login extends AppCompatActivity {
+
+  private TextView txtCreateAccount;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class Form_login extends AppCompatActivity {
     setContentView(R.layout.activity_form_login);
 
     getSupportActionBar().hide();
+    getComponentsID();
+
+    txtCreateAccount.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(Form_login.this, Form_Register.class);
+        startActivity(intent);
+      }
+    });
+  }
+
+  public void getComponentsID() {
+    txtCreateAccount = findViewById(R.id.txt_create_account);
   }
 }
