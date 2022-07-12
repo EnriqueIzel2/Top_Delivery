089e9cc5dc9de3a3e87980cd465715265cc35b68
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-11 21:19:38 -0400

    feat: add getComponentsID in onCreate

--- app/src/main/java/com/enriqueizel/topdelivery/Form_Register.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Form_Register.java
@@ -22,6 +22,8 @@ public class Form_Register extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_form_register);
+
+    getComponentsID();
   }
 
   public void getComponentsID() {
