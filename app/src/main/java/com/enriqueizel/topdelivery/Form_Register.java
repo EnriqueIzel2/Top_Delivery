ad8644838d2d784faf202e6caf5d79ba18553235
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-11 20:42:42 -0400

    feat: add TextWatcher

--- app/src/main/java/com/enriqueizel/topdelivery/Form_Register.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Form_Register.java
@@ -3,12 +3,41 @@ package com.enriqueizel.topdelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Form_Register extends AppCompatActivity {
+
+  private CircleImageView userPhoto;
+  private Button btnSelectPhoto, btnRegister;
+  private EditText editName, editEmail, editPassword;
+  private TextView txtErrorMessage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_form_register);
   }
+
+  TextWatcher registerTextWatcher = new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+  };
 }
