38919295489092309e22e87843f8bc1a0c2bd496
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-15 19:04:36 -0400

    feat: add listener to login

--- app/src/main/java/com/enriqueizel/topdelivery/Form_login.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Form_login.java
@@ -1,15 +1,25 @@
 package com.enriqueizel.topdelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Form_login extends AppCompatActivity {
 
-  private TextView txtCreateAccount;
+  private EditText editPassword, editEmail;
+  private Button btnLogin;
+  private TextView txtCreateAccount, txtErrorMessage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +36,43 @@ public class Form_login extends AppCompatActivity {
         startActivity(intent);
       }
     });
+
+    btnLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String email = editEmail.getText().toString();
+        String password = editPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+          txtErrorMessage.setText("Preencha todos os campos");
+        } else {
+          txtErrorMessage.setText("");
+        }
+      }
+    });
+  }
+
+  public void userLogin(){
+    String email = editEmail.getText().toString();
+    String password = editPassword.getText().toString();
+
+    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(
+            new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                  // TODO: 15/07/2022 leva para tela de produtos
+                }
+              }
+            }
+    );
   }
 
   public void getComponentsID() {
     txtCreateAccount = findViewById(R.id.txt_create_account);
+    btnLogin = findViewById(R.id.btn_login);
+    editEmail = findViewById(R.id.edit_email);
+    editPassword = findViewById(R.id.edit_password);
+    txtErrorMessage = findViewById(R.id.txt_error_message);
   }
 }
