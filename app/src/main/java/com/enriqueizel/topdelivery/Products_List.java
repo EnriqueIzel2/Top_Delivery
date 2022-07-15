47fe4c5a765cd801dcf3ae25ca1733cd82ef11da
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-15 19:11:33 -0400

    feat: create the products list

---
+++ app/src/main/java/com/enriqueizel/topdelivery/Products_List.java
@@ -0,0 +1,14 @@
+package com.enriqueizel.topdelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Products_List extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_products_list);
+  }
+}
