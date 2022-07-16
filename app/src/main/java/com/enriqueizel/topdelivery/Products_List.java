036c1302f6e2bf1e73ec887d0ec316467ff6addf
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-16 19:35:13 -0400

    feat: create method to log out user

--- app/src/main/java/com/enriqueizel/topdelivery/Products_List.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Products_List.java
@@ -1,9 +1,15 @@
 package com.enriqueizel.topdelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Products_List extends AppCompatActivity {
 
@@ -18,5 +24,23 @@ public class Products_List extends AppCompatActivity {
     getMenuInflater().inflate(R.menu.main_menu, menu);
 
     return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    int itemID = item.getItemId();
+
+    if (itemID == R.id.ic_user_profile) {
+
+    } else if (itemID == R.id.ic_orders) {
+
+    } else if (itemID == R.id.ic_logout) {
+      FirebaseAuth.getInstance().signOut();
+      Toast.makeText(Products_List.this, "Sessão encerrada", Toast.LENGTH_SHORT).show();
+      Intent intent = new Intent(Products_List.this, Form_login.class);
+      startActivity(intent);
+      finish();
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
