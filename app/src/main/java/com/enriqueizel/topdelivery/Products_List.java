92e09fb66253c477af28aaf2f209f28cc6eb3add
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-16 19:20:36 -0400

    feat: render menu component

--- app/src/main/java/com/enriqueizel/topdelivery/Products_List.java
+++ app/src/main/java/com/enriqueizel/topdelivery/Products_List.java
@@ -3,6 +3,7 @@ package com.enriqueizel.topdelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Products_List extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class Products_List extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_products_list);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+
+    return true;
   }
 }
