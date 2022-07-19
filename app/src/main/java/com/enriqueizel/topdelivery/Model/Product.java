a90bb61fa6c51798099dcb685f7245e8f0bd80eb
EnriqueIzel2 <eleaoizel@gmail.com>
2022-07-19 18:54:45 -0400

    feat: create Product class

---
+++ app/src/main/java/com/enriqueizel/topdelivery/Model/Product.java
@@ -0,0 +1,41 @@
+package com.enriqueizel.topdelivery.Model;
+
+public class Product {
+
+  private int photo;
+  private String name;
+  private String price;
+  private String description;
+
+  public int getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(int photo) {
+    this.photo = photo;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
